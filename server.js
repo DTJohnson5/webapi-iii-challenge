5da6c74ef4969a4a0d6479f709fb4f65ce3d9afb
David.Johnson <davidt.johnson75@yahoo.com>
2019-10-30 18:45:54 -0500

    MVP met

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = express.json();
 
 const server = express();
+const router = require('./users/userRouter');
+
+server.use(bodyParser);
+server.use(helmet());
+server.use(logger);
+
+server.use('/api/users', router);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +18,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  const date = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString();
+  next();
 };
 
 module.exports = server;
