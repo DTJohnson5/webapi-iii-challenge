8b5409a3e9069e216092ff5dc8c1281554b4b310
David.Johnson <davidt.johnson75@yahoo.com>
2019-10-31 12:42:08 -0500

    To see if Heroku will work

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const server = require("./server.js");
 
 const port = process.env.PORT || 5000;
 
+server.get('/', (req, res) => {
+    res.status(200).json({Hello: 'Web 23!'});
+})
+
 server.listen(port, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
